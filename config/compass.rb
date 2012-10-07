002c369a955fba82769a22375ae931083870d2d6
Morteza Heydari <mortezaheydari1988@gmail.com>
2012-10-07 13:34:34 +0300

    init Commit

---
+++ config/compass.rb
@@ -0,0 +1,2 @@
+# Require any additional goboom-frontend plugins here.
+project_type = :rails
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Compass::Application.config.secret_token = 'fb8c1b1fb05900337f64e743c4d9d81daba884c28ce962f3804ce4bc2e8d15ad12b3cc958221644723b6c86fc128e4192f67ea7e1024097fce5818c8cf86598f'
